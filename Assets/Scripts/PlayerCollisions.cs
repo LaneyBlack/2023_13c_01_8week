6b31df63dc93c3d3d311735e363febc49d8e68c8
jarek-as <jarekfbad@gmail.com>
2023-10-11 17:29:35 +0200

    divide player into two (nested prefabs)

---
+++ Assets/Scripts/PlayerCollisions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerCollisions : MonoBehaviour
+    {
+        private BasicPlayerMovement _movement;
+        private void Awake()
+        {
+            _movement = GetComponentInChildren<BasicPlayerMovement>();
+        }
+
+        
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            _movement._isGrounded = true;
+        }
+
+      
+        private void OnCollisionExit2D(Collision2D col)
+        {
+            _movement._isGrounded = false;
+        }
+    }
+}
