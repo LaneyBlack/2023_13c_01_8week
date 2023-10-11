6afbb6687b6392213bddc0e74759fbdd2e9dd14a
s24468 <s24468@pjwstk.edu.pl>
2023-10-11 21:18:35 +0200

    added sprite pirate and its rotation sprite
    we used scale -x to just change the sprite direction.
    We added mainly one script

--- Assets/Scripts/BasicPlayerMovement.cs
+++ Assets/Scripts/BasicPlayerMovement.cs
@@ -9,9 +9,11 @@ using UnityEngine;
 public class BasicPlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
+    private Transform _transform;
     private float _xInput;
     private bool _performJump;
     public bool _isGrounded;
+    public Animator Animator;
     
     [SerializeField] private float _speed = 10;
     [SerializeField] private float _jumpForce = 10;
@@ -19,6 +21,9 @@ public class BasicPlayerMovement : MonoBehaviour
     private void Awake()
     {
         _rigidbody = GetComponentInParent<Rigidbody2D>();
+        // _transform = GetComponentInParent<Transform>();
+        _transform = transform.parent;
+        Animator = GetComponentInParent<Animator>();
     }
 
 
@@ -26,10 +31,19 @@ public class BasicPlayerMovement : MonoBehaviour
     private void Update()
     {
         _xInput = Input.GetAxis("Horizontal");
-
         if (Input.GetKey(KeyCode.Space))
         {
             _performJump = true;
+        }
+
+        if (_xInput > 0)
+        {
+            _transform.localScale = new Vector3(5,5,0);
+        }
+        
+        if (_xInput < 0)
+        {
+            _transform.localScale = new Vector3(-5,5,0);
         }
     }
 
@@ -42,6 +56,7 @@ public class BasicPlayerMovement : MonoBehaviour
             _performJump = false;
             _rigidbody.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
         }
+        
     }
 
 }
