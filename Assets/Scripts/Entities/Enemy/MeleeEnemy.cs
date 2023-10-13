233525160b7bf4b651f1fbcd329eb3ca1af15d55
LaneyBlack-Laptop <laney.black.74@gmail.com>
2023-10-13 22:47:18 +0200

    Hit Fov done.

--- Assets/Scripts/Entities/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Entities/Enemy/MeleeEnemy.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 
 public class MeleeEnemy : MonoBehaviour
 {
-    [SerializeField] private float attackCooldown;
     [SerializeField] private float damage;
-    [SerializeField] private float speed;
-    [SerializeField] private BoxCollider2D boxCollider;
-    private float _cooldownTimer = 0;
+    [SerializeField] private float movementSpeed;
 
-    // [SerializeField] private float _speed;
+    // Hit Fov ---
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private float hitFovRange;
+    [SerializeField] private float hitFovDistance;
+    [SerializeField] private LayerMask layerMask;
+    
+    [SerializeField] private float attackCooldown;
+    private float _cooldownTimer;
+
     private Rigidbody2D _rigidbody;
-    private BoxCollider2D _boxCollider;
+    
+    private Animator _animator;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,6 +35,7 @@ public class MeleeEnemy : MonoBehaviour
         {
             // Attack
             _cooldownTimer = attackCooldown;
+            _animator.SetTrigger("meleeAttack");
         }
     }
     
@@ -38,10 +46,21 @@ public class MeleeEnemy : MonoBehaviour
 
     private bool IsPlayerInSight()
     {
-        // RaycastHit2D hitFov = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size);
-        
-        // Here I need to create a new collider
-        return false;   
+        // Origin, Size, Angle, direction
+        var hitFov = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * (hitFovDistance * transform.localScale.x), //
+            new Vector3(boxCollider.bounds.size.x * hitFovRange, boxCollider.bounds.size.y, boxCollider.bounds.size.z), // size x depends on range
+            0,Vector2.left,
+            0,layerMask);
+        return hitFov.collider!=null;
+    }
+
+    // For Debugging purposes
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        // Draw Hit Fov
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * (hitFovDistance * transform.localScale.x),
+            new Vector2(boxCollider.bounds.size.x * hitFovRange, boxCollider.bounds.size.y));
     }
     
 }
