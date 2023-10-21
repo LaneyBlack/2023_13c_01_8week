788e295d3f10a79651169d9451361565d30ada16
Bartosz Mielczarek <95695462+barmiel1@users.noreply.github.com>
2023-10-21 03:16:18 +0200

    added grapple point prefab
    turns green when player can attach the hook

---
+++ Assets/Prefabs/Hook/GrapplePoint.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplePoint : MonoBehaviour
+{
+    private float                   promptRadius;           //distance when "can attach" color starts to change       
+    private SpriteRenderer          spriteRenderer;
+    [SerializeField] private float  attachRadius = 10f;     //how close player must be to the point to attach himself
+    /*[SerializeField]*/ private Color  canAttachColor = Color.green;
+    /*[SerializeField]*/ private Color  defaultColor = Color.red;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        promptRadius = attachRadius * 1.5f;
+    }
+
+    private void Update()
+    {
+        var player = GameObject.FindWithTag("Player");
+
+        if (player != null) 
+        {
+            float distance = Vector2.Distance(player.transform.position, transform.position);
+            Debug.Log("player found. Distance = " + distance);
+
+            spriteRenderer.color = Color.Lerp(canAttachColor, defaultColor, (distance - attachRadius) / (promptRadius - attachRadius));
+        }
+    }
+}
