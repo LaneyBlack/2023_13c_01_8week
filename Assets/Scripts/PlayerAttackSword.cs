5ddf7fe12e5e56c72b2588c8d94ad52b6505822f
jarek-as <jarekfbad@gmail.com>
2023-10-21 16:33:13 +0200

    meele player attack
    in this commit I also left a debug.Log that says how much certain enemy has  current healt.
    
    It was necessary, because enemy hadn't have animation of death, so it was impossible to tell is this commit works or not

--- Assets/Scripts/PlayerAttackSword.cs
+++ Assets/Scripts/PlayerAttackSword.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,36 @@ public class PlayerAttackSword : MonoBehaviour
     // Start is called before the first frame update
     public Animator animator;
     [SerializeField] private KeyCode _keyCode;
+    [SerializeField] public Transform attackPoint;
+    [SerializeField] public float attackRange = 0.5f;
+    [SerializeField] public int damageValue = 1;
+    public LayerMask enemyLayers;
+    
     void Update()
     {
         if (Input.GetKeyDown(_keyCode))
         {
             animator.SetTrigger("AttackSword");
-
+            attack();
         }
     }
 
+    void attack()
+    {
+       Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position,attackRange,enemyLayers);
+       foreach (Collider2D enemy in hitEnemies)
+       {
+           Debug.Log("We hit and its currenntHealth ="+enemy.GetComponent<Health>().currentHealth);
+           enemy.GetComponent<Health>().takeDamage(damageValue);
+       }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position,attackRange);
+    }
 }
