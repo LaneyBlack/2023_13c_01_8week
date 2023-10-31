46dedeb3de27ed92a3d20f43778169c411d25e29
Bartosz Mielczarek <95695462+barmiel1@users.noreply.github.com>
2023-11-01 00:40:08 +0100

    moved all movement code to player movement script
    that is: whole code from PlayerCollision and from PlayerMovementAnim

--- Assets/Scripts/Player/PlayerMovementAnim.cs
+++ Assets/Scripts/Player/PlayerMovementAnim.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class PlayerMovementAnim : MonoBehaviour
 {
-    public Animator _Animator;
-    public SpriteRenderer _spriteRenderer;
+    //public Animator _Animator;
+    //public SpriteRenderer _spriteRenderer;
 
-    private void Start()
-    {
-       // _spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+    //private void Start()
+    //{
+    //   // _spriteRenderer = GetComponent<SpriteRenderer>();
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        _Animator.SetBool("Run", Input.GetAxis("Horizontal") != 0);
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    _Animator.SetBool("Run", Input.GetAxis("Horizontal") != 0);
 
-        float xInput = Input.GetAxis("Horizontal");
-        if (xInput > 0)
-        {
-            _spriteRenderer.flipX = false;
-        }
-        else if (xInput < 0)
-        {
-            _spriteRenderer.flipX = true;
-        }
-    }
+    //    float xInput = Input.GetAxis("Horizontal");
+    //    if (xInput > 0)
+    //    {
+    //        _spriteRenderer.flipX = false;
+    //    }
+    //    else if (xInput < 0)
+    //    {
+    //        _spriteRenderer.flipX = true;
+    //    }
+    //}
 }
