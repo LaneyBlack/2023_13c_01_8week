fe4311be5c60fab6754245ca83b7c32028ab4a6f
s24468 <s24468@pjwstk.edu.pl>
2023-11-03 00:40:56 +0100

    Key and chest
    opening chest with specific id

---
+++ Assets/Scripts/Collectibles/ChestOpener.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ChestOpener : MonoBehaviour
+{
+    [SerializeField] private int id = 0;
+    public Animator animator;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && InvenoryManagment.KeysId.Contains(id))
+        {
+            anim.SetTrigger("OpenChest");
+            Destroy(gameObject, 2f);
+        }
+    }
+}
