197f2cd592368975288dbbbc08f1f1452c1338c5
Bartosz Mielczarek <95695462+barmiel1@users.noreply.github.com>
2023-11-03 00:40:46 +0100

    added inventory UI

---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public KeyCode code;
+    public Sprite sprite;
+
+    public Item(KeyCode code, Sprite sprite)
+    {
+        this.code = code;
+        this.sprite = sprite;
+    }
+}
+
+public class Inventory : MonoBehaviour
+{
+    [Header("Weapon setup")]
+    [SerializeField] private KeyCode weaponKey;
+    [SerializeField] private Sprite weaponIcon;
+
+
+    [Header("Hook setup")]
+    [SerializeField] private KeyCode hookKey;
+    [SerializeField] private Sprite HookIcon;
+
+
+    [Header("Potion setup")]
+    [SerializeField] private KeyCode potionKey;
+    [SerializeField] private Sprite potionIcon;
+
+    public List<Item> itemsData;
+
+    private void Awake()
+    {
+        itemsData = new List<Item>
+        {
+            new Item(weaponKey, weaponIcon),
+            new Item(hookKey, HookIcon),
+            new Item(potionKey, potionIcon)
+        };
+    }
+}
