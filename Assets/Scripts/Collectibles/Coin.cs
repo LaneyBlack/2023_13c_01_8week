2e8ea2f23933307592f4ccc581cdaec8fb099cf1
jarek-as <jarekfbad@gmail.com>
2023-11-02 20:09:49 +0100

    moneyManagement
    two scripts with Debug.Log in Coin.cs
    Coin.cs
    MoneyManagement.cs

---
+++ Assets/Scripts/Collectibles/Coin.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private int value = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            MoneyManagment.NumberOfCoins+=value;
+            Debug.LogError(MoneyManagment.NumberOfCoins);
+            Destroy(gameObject);
+        }
+    }
+}
