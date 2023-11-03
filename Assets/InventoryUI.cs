6fd57b17bfdb64e0ae9908c0147fe419489078d0
Bartosz Mielczarek <95695462+barmiel1@users.noreply.github.com>
2023-11-03 03:50:57 +0100

    small inventory UI update

--- Assets/InventoryUI.cs
+++ Assets/InventoryUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -24,12 +25,13 @@ public class InventoryUI : MonoBehaviour
                 x.sprite = inventory.itemsData[itemid++].sprite;
         }
 
+        string pattern = "Keypad|Alpha";
         for (int i = 0, itemid = 0; i < texts.Length; i++)
         {
             var x = texts[i];
 
             if (x.gameObject.CompareTag("UI_Key"))
-                x.text = inventory.itemsData[itemid++].code.ToString().Replace("Keypad", "");
+                x.text = Regex.Replace(inventory.itemsData[itemid++].keycode.ToString(), pattern, "");
         }
     }
 }
