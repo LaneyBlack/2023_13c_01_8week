1436d5b31f755c9321116e5cf16d7f9e370b1805
jarek-as <jarekfbad@gmail.com>
2023-11-12 18:16:17 +0100

    8th part boss v1

--- Assets/Scripts/Boss/BossAttack.cs
+++ Assets/Scripts/Boss/BossAttack.cs
@@ -12,13 +12,14 @@ public class BossAttack : MonoBehaviour
     [SerializeField] private GameObject waterProjectile;
     private GameObject player;
     [SerializeField] private float attackGrownRange = 3f;
-    [SerializeField] private float attackSmallRange = 2f;
+    [SerializeField] private float attackSmallRange = 4f;
     [SerializeField] private float attackGrownCooldown = 2f; // Cooldown between attacks
     [SerializeField] private float attackSmallCooldown = 1.5f; // Cooldown between attacks
     [SerializeField] private BossMovement bossMovement;
     [SerializeField] public Transform attackPoint;
 
     public LayerMask playerLayers;
+
     private float
         _cooldownTimer = Mathf.Infinity;
 
@@ -34,11 +35,16 @@ public class BossAttack : MonoBehaviour
         float distanceToPlayer = Vector2.Distance(transform.parent.position, player.transform.position);
         if (SmallBossVisuals.activeSelf)
         {
-            if (distanceToPlayer <= attackSmallRange && _cooldownTimer >= attackSmallCooldown)
+            if (distanceToPlayer <= attackSmallRange/4 && _cooldownTimer >= attackSmallCooldown)
             {
                 Attack();
                 _cooldownTimer = 0f;
             }
+            // else if (distanceToPlayer <= attackSmallRange && _cooldownTimer >= attackSmallCooldown)
+            // {
+            //     Small_Attack_2();
+            //     _cooldownTimer = 0f;
+            // }
         }
         else
         {
@@ -48,8 +54,14 @@ public class BossAttack : MonoBehaviour
                 _cooldownTimer = 0f;
             }
         }
-
     }
+
+    // void Small_Attack_2()
+    // {
+    //     bossMovement.canMove = false;
+    //     _SmallBossAnimator.SetTrigger("Attack_small_boss");
+    //     StartCoroutine(AppearBubble());
+    // }
 
     // ReSharper disable Unity.PerformanceAnalysis
     void Attack()
@@ -69,6 +81,39 @@ public class BossAttack : MonoBehaviour
     }
 
     // ReSharper disable Unity.PerformanceAnalysis
+    // private IEnumerator AppearBubble()
+    // {
+    //     yield return new WaitForSeconds(0.3f);
+    //     waterProjectile.SetActive(true);
+    //     float time = 0;
+    //     Vector3 originalScale = waterProjectile.transform.localScale;
+    //
+    //     while (time < 1f)
+    //     {
+    //         bool isFlip = (transform.parent.position.x - player.transform.position.x) < 0;
+    //         waterProjectile.transform.localScale += new Vector3(0.007f, 0, 0);
+    //         waterProjectile.GetComponent<SpriteRenderer>().flipX = isFlip;
+    //         if (isFlip)
+    //         {
+    //             waterProjectile.transform.localPosition = new Vector3(-0.07f, 0, 0); //prawo
+    //
+    //             waterProjectile.GetComponentInChildren<WaterProjectile>().changePosition(0.15f, true);
+    //         }
+    //         else
+    //         {
+    //             waterProjectile.transform.localPosition = new Vector3(0.04f, 0, 0);
+    //             waterProjectile.GetComponentInChildren<WaterProjectile>().changePosition(-0.15f, false);
+    //         }
+    //
+    //         yield return null;
+    //         time += Time.deltaTime;
+    //     }
+    //
+    //     waterProjectile.transform.localScale = originalScale;
+    //     waterProjectile.SetActive(false);
+    //     bossMovement.canMove = true;
+    // }
+
     private IEnumerator AppearProjectile()
     {
         yield return new WaitForSeconds(0.3f);
@@ -79,18 +124,17 @@ public class BossAttack : MonoBehaviour
         while (time < 1f)
         {
             bool isFlip = (transform.parent.position.x - player.transform.position.x) < 0;
-            waterProjectile.transform.localScale +=  new Vector3(0.007f, 0, 0); 
+            waterProjectile.transform.localScale += new Vector3(0.007f, 0, 0);
             waterProjectile.GetComponent<SpriteRenderer>().flipX = isFlip;
             if (isFlip)
             {
-                waterProjectile.transform.localPosition =  new Vector3(-0.07f, 0, 0); //prawo
+                waterProjectile.transform.localPosition = new Vector3(-0.07f, 0, 0); //prawo
 
                 waterProjectile.GetComponentInChildren<WaterProjectile>().changePosition(0.15f, true);
-
             }
             else
             {
-                waterProjectile.transform.localPosition =  new Vector3(0.04f, 0, 0); 
+                waterProjectile.transform.localPosition = new Vector3(0.04f, 0, 0);
                 waterProjectile.GetComponentInChildren<WaterProjectile>().changePosition(-0.15f, false);
             }
 
@@ -102,27 +146,31 @@ public class BossAttack : MonoBehaviour
         waterProjectile.SetActive(false);
         bossMovement.canMove = true;
     }
+
     private IEnumerator SmallAttackAnimationDuration()
     {
         yield return new WaitForSeconds(0.5f);
-        
+
         bossMovement.canMove = true;
     }
+
     void SmallAttackColliders()
     {
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position,1.4f,playerLayers);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, 1.4f, playerLayers);
         foreach (Collider2D enemy in hitEnemies)
         {
-            Debug.Log("We hit and its currenntHealth ="+enemy.GetComponent<Health>().CurrentHealth);
+            Debug.Log("We hit and its currenntHealth =" + enemy.GetComponent<Health>().CurrentHealth);
             enemy.GetComponent<Health>().TakeDamage(1);
         }
     }
+
     private void OnDrawGizmosSelected()
     {
         if (attackPoint == null)
         {
             return;
         }
-        Gizmos.DrawWireSphere(attackPoint.position,1.4f);
+
+        Gizmos.DrawWireSphere(attackPoint.position, 1.4f);
     }
 }
