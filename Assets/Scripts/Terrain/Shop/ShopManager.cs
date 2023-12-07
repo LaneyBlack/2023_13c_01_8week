813d3e418a846567c6bf29c1d87c99e5f7e5a849
Antoni Szot <s25074@pjwstk.edu.pl>
2023-12-07 14:25:58 +0100

    Added prices to shop

--- Assets/Scripts/Terrain/Shop/ShopManager.cs
+++ Assets/Scripts/Terrain/Shop/ShopManager.cs
@@ -7,18 +7,50 @@ public class ShopManager : MonoBehaviour
     [SerializeField] private GameObject _shopUI;
     [SerializeField] private Button _buyPotionButton;
     [SerializeField] private Button _buySwordButton;
+    [SerializeField] private TextMeshProUGUI potionPriceText;
+    [SerializeField] private TextMeshProUGUI swordPriceText;
+
+    private int potionPrice = 20;
+    private int swordPrice = 50;
 
     private void Start()
     {
         //InvenoryManagment.NumberOfCoins = 1000;
         _buyPotionButton.onClick.AddListener(BuyPotion);
         _buySwordButton.onClick.AddListener(BuySword);
+        
+        UpdatePotionPriceText();
+        UpdateSwordPriceText();
+        
+        _shopUI.SetActive(false);
+    }
+
+    private void UpdatePotionPriceText()
+    {
+        if (potionPriceText != null)
+        {
+            potionPriceText.text = potionPrice.ToString();
+        }
+        else
+        {
+            Debug.LogError("PotionPrice TextMeshProUGUI component not assigned!");
+        }
+    }
+    
+    private void UpdateSwordPriceText()
+    {
+        if (swordPriceText != null)
+        {
+            swordPriceText.text = swordPrice.ToString();
+        }
+        else
+        {
+            Debug.LogError("PotionPrice TextMeshProUGUI component not assigned!");
+        }
     }
 
     private void BuyPotion()
     {
-        int potionPrice = 20;
-
         if (InvenoryManagment.NumberOfCoins >= potionPrice)
         {
             InvenoryManagment.NumberOfCoins -= potionPrice;
@@ -32,8 +64,6 @@ public class ShopManager : MonoBehaviour
 
     private void BuySword()
     {
-        int swordPrice = 50;
-
         if (InvenoryManagment.NumberOfCoins >= swordPrice)
         {
             InvenoryManagment.NumberOfCoins -= swordPrice;
